0ca15555dbb63ee47b64cfca44a98a95f01cfb25
Starr Lab Beast <roeegilron@gmail.com>
2018-04-18 13:08:55 -0700

    Edited setup.py, was throwing errors. Edited name of package in environment.yml.

--- img_pipe/setup.py
+++ img_pipe/setup.py
@@ -21,8 +21,8 @@ import os, subprocess
 setup(
     name='img_pype',
     version='1.0',
-    packages=['', 'plotting', 'SupplementalFiles', 'SupplementalScripts', 'surface_warping_scripts',
-              'electrode-registration-app', 'electrode-registration-app.Old', 'electrode-registration-app.app',
+    packages=['plotting', 'SupplementalFiles', 'SupplementalScripts', 'surface_warping_scripts',
+              'electrode-registration-app','electrode-registration-app.app',
               'electrode-registration-app.app.ui', 'electrode-registration-app.app.core',
               'electrode-registration-app.tests'],
     url='https://github.com/MichaelLebrand/img_pype',
